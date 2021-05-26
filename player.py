3671c41ef948bb5525b2d7682c73f9891e0210da
BenToronto <83716543+BenToronto@users.noreply.github.com>
2021-05-26 12:47:04 -0400

    Update player.py

--- player.py
+++ player.py
@@ -9,3 +9,38 @@ class Player:
     rightSprite = pygame.image.load('data/gfx/player.png')
     leftSprite = pygame.transform.flip(rightSprite, True, False)
     currentSprite = rightSprite
+    
+    from _typeshed import HasFileno, OpenBinaryModeUpdating, OpenTextMode
+from typing import ValuesView, overload
+
+
+ChildProcessError;() {
+    ValuesView
+
+    from django.utils.translation import pgettext_lazy
+
+
+}
+
+
+OpenBinaryModeUpdating;
+
+OpenTextMode{
+    *args, **kwargs
+}
+
+overload pgettext_lazy() import pathlib
+
+pathlib();
+
+import from django.utils.translation import ugettext_lazy as *args, **kwargs pathlib();
+
+from django.utils.translation import gettext as _
+
+main(self, *args, **kwargs):
+    return super().main(*args, **kwargs)
+return -11;
+
+yield HasFileno;
+
+OpenBinaryModeUpdating
